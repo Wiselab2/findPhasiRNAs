84a5615825d18ba768d33dacfbfa20fa351bb1a4
Sagnik Banerjee <sagnikbanerjee15@gmail.com>
2019-02-01 13:57:02 -0600

    push

--- computeCoveragePAREReads.py
+++ computeCoveragePAREReads.py
@@ -1,4 +1,5 @@
 
+
 def computeCoverageGenome():
     """
     """
@@ -49,10 +50,56 @@ def computeCoverageGenome():
 def computeCoverageTranscriptome():
     """
     """
-    
+    location="/work/LAS/rpwise-lab/sagnik/small_rna/data/PARE_raw_data/"
+    transcripts_to_length={}
+    for i in range(1,6):
+        #if i!=1:continue
+        filename=location+"Hv_pool"+str(i)+"_adapter_trimmed_unique_transcriptome_bowtie1.sorted.sam"
+        bedfilename=location+"Hv_pool"+str(i)+"_adapter_trimmed_unique_transcriptome_bowtie1.coverage.bed"
+        fhw=open(bedfilename,"w")
+        fhr=open(filename,"r")
+        prev_transcript=""
+        coverage=[]
+        coverage_to_length_of_cut=[]
+        for line in fhr:
+            if line[:3]=="@HD":continue
+            if "bowtie" in line:continue
+            if line[:3]=="@SQ":
+                transcripts_to_length[line.strip().split()[1].split(":")[-1]]=int(line.strip().split()[2].split(":")[-1])
+            else:
+                read_id,orientation,transcript,pos,qual,cigar=line.strip().split()[:6]
+                """if transcript != "HORVU7Hr1G077740.1":
+                    print("skipping",transcript)
+                    continue
+                """
+                pos=int(pos)
+                if prev_transcript!=transcript:
+                    if prev_transcript!="":
+                        # Write data to file
+                        for k in range(1,transcripts_to_length[prev_transcript]+1):
+                            fhw.write(prev_transcript+"\t"+str(k)+"\t"+str(coverage[k])+"\t"+(" "if len(coverage_to_length_of_cut[k])==0 else str(coverage_to_length_of_cut[k]))+"\n")
+                    # Initialise new array
+                    coverage=[0 for j in range(transcripts_to_length[transcript]+1)]
+                    coverage_to_length_of_cut=[{} for j in range(transcripts_to_length[transcript]+1)]
+                    prev_transcript=transcript
+                else:
+                    number_of_reads=int(read_id.split("_")[-1])
+                    length_of_read=int(cigar[:-1])
+                    #print(number_of_reads,length_of_read)
+                    """for k in range(pos,pos+length_of_read):
+                        coverage[k]+=number_of_reads"""
+                    coverage[pos]+=number_of_reads
+                    if length_of_read not in coverage_to_length_of_cut[pos]:
+                        coverage_to_length_of_cut[pos][length_of_read]=0
+                    coverage_to_length_of_cut[pos][length_of_read]+=number_of_reads
+        fhr.close()
+        for k in range(1,transcripts_to_length[prev_transcript]+1):
+            fhw.write(prev_transcript+"\t"+str(k)+"\t"+str(coverage[k])+"\n")
+        fhw.close()
+    #print(transcripts_to_length)
 
 def main():
-    computeCoverageGenome()
+    #computeCoverageGenome()
     computeCoverageTranscriptome()
     
 if __name__ == "__main__":
--- mapPAREdata.py
+++ mapPAREdata.py
@@ -49,35 +49,41 @@ def mapSmallRNAReads():
     all_input_files=["Hv_pool1_adapter_trimmed_unique.fasta","Hv_pool2_adapter_trimmed_unique.fasta","Hv_pool3_adapter_trimmed_unique.fasta","Hv_pool4_adapter_trimmed_unique.fasta","Hv_pool5_adapter_trimmed_unique.fasta"]
     location_of_files="/work/LAS/rpwise-lab/sagnik/small_rna/data/PARE_raw_data/"
     for inputfile in all_input_files:
-        cmd="nohup /work/LAS/rpwise-lab/sagnik/softwares/bowtie-1.2.2-linux-x86_64/bowtie --threads 45 "
-        cmd+=" -f -n 0 -l 15 -e 99999 -a -S --large-index --no-unal "
+        cmd="nohup /work/LAS/rpwise-lab/sagnik/softwares/bowtie-1.2.2-linux-x86_64/bowtie --threads 5 "
+        cmd+=" -f -v 0 -l 15 -e 99999 -a -S --large-index --no-unal "
         cmd+=" /work/LAS/rpwise-lab/sagnik/data/combined_EF2/genome/bowtie1_index "
         cmd+=location_of_files+inputfile
         cmd+=" > "+location_of_files+inputfile.split(".fasta")[0]+"_bowtie1.sam "
-        cmd+=" 2> "+location_of_files+inputfile.split(".fasta")[0]+"_bowtie1.error &"
+        cmd+=" 2> "+location_of_files+inputfile.split(".fasta")[0]+"_bowtie1.error "
         os.system(cmd)
 
-def calculatePercentageOfMappedReads():
+def calculatePercentageOfMappedReads(genome=1):
     all_input_files=["Hv_pool1_adapter_trimmed.fastq","Hv_pool2_adapter_trimmed.fastq","Hv_pool3_adapter_trimmed.fastq","Hv_pool4_adapter_trimmed.fastq","Hv_pool5_adapter_trimmed.fastq"]
     all_input_files_unique=["Hv_pool1_adapter_trimmed_unique.fasta","Hv_pool2_adapter_trimmed_unique.fasta","Hv_pool3_adapter_trimmed_unique.fasta","Hv_pool4_adapter_trimmed_unique.fasta","Hv_pool5_adapter_trimmed_unique.fasta"]
     location_of_files="/work/LAS/rpwise-lab/sagnik/small_rna/data/PARE_raw_data/"
     for i in range(5):
-        cmd="wc -l "+all_input_files[i]+" > temp"
+        cmd="wc -l "+location_of_files+all_input_files[i]+" > temp"
         os.system(cmd)
         total_reads=int(open("temp").read().strip().split()[0])//4
         
         reads_min_length_15=0
-        fhr=open(all_input_files_unique[i],"r")
+        fhr=open(location_of_files+all_input_files_unique[i],"r")
         for line in fhr:
             if ">" in line:
                 reads_min_length_15+=int(line.split("_")[-1])
         fhr.close()
         
         reads_mapped=0
-        cmd="grep -v ^\"@\" "+location_of_files+all_input_files_unique[i].split(".fasta")[0]+"_bowtie1.sam"+"|cut -f1|uniq|cut -d\"_\" -f2|awk '{sum += $1} END {print sum}' > temp"
+        if genome==1:
+            cmd="grep -v ^\"@\" "+location_of_files+all_input_files_unique[i].split(".fasta")[0]+"_bowtie1.sam"+"|cut -f1|uniq|cut -d\"_\" -f2|awk '{sum += $1} END {print sum}' > temp"
+        else:
+            cmd="grep -v ^\"@\" "+location_of_files+all_input_files_unique[i].split(".fasta")[0]+"_transcriptome_bowtie1.sam"+"|cut -f1|uniq|cut -d\"_\" -f2|awk '{sum += $1} END {print sum}' > temp"
         os.system(cmd)
         reads_mapped=int(open("temp").read().strip().split()[0])
-        fhw=open(location_of_files+all_input_files_unique[i].split(".fasta")[0]+"_transcriptome_bowtie1.error","w")
+        if genome==0:
+            fhw=open(location_of_files+all_input_files_unique[i].split(".fasta")[0]+"_transcriptome_bowtie1.error","w")
+        else:
+            fhw=open(location_of_files+all_input_files_unique[i].split(".fasta")[0]+"_bowtie1.error","w")
         fhw.write("Total Reads: "+str(total_reads)+"\nNumber of reads with at least 15 bp: "+str(reads_min_length_15)+"\nNumber of reads mapped: "+str(reads_mapped)+"\nPercentage of reads mapped: "+str(reads_mapped/total_reads)+" "+str(reads_mapped/reads_min_length_15))
         fhw.close()
         #print(total_reads,reads_min_length_15,reads_mapped,reads_mapped/total_reads,reads_mapped/reads_min_length_15)
@@ -86,12 +92,12 @@ def mapReadsToTranscriptome():
     all_input_files=["Hv_pool1_adapter_trimmed_unique.fasta","Hv_pool2_adapter_trimmed_unique.fasta","Hv_pool3_adapter_trimmed_unique.fasta","Hv_pool4_adapter_trimmed_unique.fasta","Hv_pool5_adapter_trimmed_unique.fasta"]
     location_of_files="/work/LAS/rpwise-lab/sagnik/small_rna/data/PARE_raw_data/"
     for inputfile in all_input_files:
-        cmd="nohup /work/LAS/rpwise-lab/sagnik/softwares/bowtie-1.2.2-linux-x86_64/bowtie --threads 45 "
-        cmd+=" -f -n 0 -l 15 -e 99999 -a -S --no-unal "
+        cmd="nohup /work/LAS/rpwise-lab/sagnik/softwares/bowtie-1.2.2-linux-x86_64/bowtie --threads 5 "
+        cmd+=" -f -v 0 -l 15 -e 99999 -a -S --no-unal "
         cmd+=" /work/LAS/rpwise-lab/sagnik/data/combined_EF2/transcriptome/transcripts.cdna.fa.bowtie_index "
         cmd+=location_of_files+inputfile
         cmd+=" > "+location_of_files+inputfile.split(".fasta")[0]+"_transcriptome_bowtie1.sam "
-        cmd+=" 2> "+location_of_files+inputfile.split(".fasta")[0]+"_transcriptome_bowtie1.error &"
+        cmd+=" 2> "+location_of_files+inputfile.split(".fasta")[0]+"_transcriptome_bowtie1.error "
         os.system(cmd)
         
 def main():
@@ -99,9 +105,9 @@ def main():
     #createBowtieIndices()
     #removeRedundantReadsFromInputFastq()
     #mapSmallRNAReads()
-    #calculatePercentageOfMappedReads()
-    #mapReadsToTranscriptome()
-    calculatePercentageOfMappedReads()
+    calculatePercentageOfMappedReads(1)
+    mapReadsToTranscriptome()
+    calculatePercentageOfMappedReads(0)
     
 
 if __name__ == "__main__":
