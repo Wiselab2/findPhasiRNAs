a821896e4e8aeba041598ce5f7a71b7eba4cc1a4
Sagnik <sagnikbanerjee15@gmail.com>
2019-03-03 07:43:09 -0600

    Update plot.R

--- plot.R
+++ plot.R
@@ -13,33 +13,6 @@ cycle = args[3]
 phasing_score_files = list.files(path = output_directory, pattern = "*.phasing_score")
 abundance_files = list.files(path = output_directory, pattern = "*.abundance")
 
-
-multiplot <- function(..., plotlist=NULL, file, cols=1, layout=NULL) 
-{
-	plots <- c(list(...), plotlist)
-	numPlots = length(plots)
-	if (is.null(layout)) 
-	{
-		layout <- matrix(seq(1, cols * ceiling(numPlots/cols)),
-				ncol = cols, nrow = ceiling(numPlots/cols))
-	}
-	if (numPlots==1) 
-	{
-		print(plots[[1]])
-	} 
-	else 
-	{
-		grid.newpage()
-		pushViewport(viewport(layout = grid.layout(nrow(layout), ncol(layout))))
-		for (i in 1:numPlots) 
-		{
-			matchidx <- as.data.frame(which(layout == i, arr.ind = TRUE))
-			print(plots[[i]], vp = viewport(layout.pos.row = matchidx$row,
-							layout.pos.col = matchidx$col))
-		}
-	}
-}
-
 graph_list<-list()
 i<-1
 
